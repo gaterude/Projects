3664d0f44a97b2770debf9c037404d711166df1b
Michira <gaterudemichira@gmail.com>
2026-05-16 18:07:50 +0300

    integrate components into App

---
+++ api/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter";
+import Toggle from "./components/Toggle";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+
+    <Toggle/>
+    
+    </div>
+  );
+}
+
+export default App;
