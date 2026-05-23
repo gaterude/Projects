ba2afea7ee8d4fb8601a0deba8e2a9f59c6ae9df
Michira <gaterudemichira@gmail.com>
2026-05-23 08:53:24 +0300

    feat:complete form

---
+++ Signupform/src/App.jsx
@@ -0,0 +1,12 @@
+import SignupForm from "./Components/SignupForm";
+function App() {
+  return (
+    <div>
+      <h1>Signup Form</h1>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
+
